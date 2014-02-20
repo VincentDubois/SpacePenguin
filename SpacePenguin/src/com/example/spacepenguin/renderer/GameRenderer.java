6fc9367de3e6de98ee946223e0b9fa50fb7959f1
Justin Tonnel <justin.tonnel@gmail.com>
2014-02-20 11:26:59 +0100

    asteroid + fond changé

--- SpacePenguin/src/com/example/spacepenguin/renderer/GameRenderer.java
+++ SpacePenguin/src/com/example/spacepenguin/renderer/GameRenderer.java
@@ -199,7 +199,7 @@ public class GameRenderer implements GLSurfaceView.Renderer {
 //		generateCubes();			
 
 		// Set the background clear color to black.
-		GLES20.glClearColor(0f, 0f, 0f, 0f);
+		GLES20.glClearColor(0f, 0f, 0.3f, 1f);
 
 		// Use culling to remove back faces.
 		GLES20.glEnable(GLES20.GL_CULL_FACE);
